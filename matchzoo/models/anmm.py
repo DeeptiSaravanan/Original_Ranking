b3f7adc83ea7d311817870b13f987adbd4320fd3
Deep98 <deep02matrix@gmail.com>
2021-03-22 15:55:21 +0530

    Update anmm.py

--- matchzoo/models/anmm.py
+++ matchzoo/models/anmm.py
@@ -51,7 +51,7 @@ class ANMM(BaseModel):
         """
         # query is [batch_size, left_text_len]
         # doc is [batch_size, right_text_len, bin_num]
-        query, doc, d_one_tensors = self._make_inputs()
+        query, doc, d_one_tensors, q_one_tensors = self._make_inputs()
         #embedding = self._make_embedding_layer()
 
         #q_embed = embedding(query)
@@ -71,7 +71,7 @@ class ANMM(BaseModel):
             d_bin)
         
         score0 = tensorflow.keras.layers.Dot(axes=[1, 1])([d_bin, d_one_tensors])
-       
+       '''
         q_embed0 = tensorflow.keras.layers.Dropout(
             rate=self._params['dropout_rate'])(query)
         
@@ -93,3 +93,33 @@ class ANMM(BaseModel):
         score = tensorflow.keras.layers.Dot(axes=[1, 1])([q_attention, q_embed1])
         x_out = self._make_output_layer()(score)
         self._backend = keras.Model(inputs=[query, doc, d_one_tensors], outputs=x_out)
+        '''
+    
+        q_embed0 = tensorflow.keras.layers.Dropout(
+            rate=self._params['dropout_rate'])(query)
+            
+        #frequency vector
+        
+        q_embed1 = tensorflow.keras.layers.Dropout(
+            rate=self._params['dropout_rate'])(score0)
+        
+        #d_bin1 = tensorflow.cast(d_bin1, tensorflow.int32)
+        
+        #d_bin1 = tensorflow.keras.layers.Reshape((d_bin0,))(d_bin1)
+            
+        q_embed = tensorflow.keras.layers.Concatenate()([q_embed0, q_embed1])
+        
+        q_attention = tensorflow.keras.layers.Dense(
+            1, kernel_initializer=RandomUniform(), use_bias=False)(q_embed)
+        q_text_len = self._params['input_shapes'][0][0]
+
+        q_attention = tensorflow.keras.layers.Lambda(
+            lambda x: softmax(x, axis=1),
+            output_shape=(q_text_len,)
+        )(q_attention)
+        
+        # Score 1
+        score = tensorflow.keras.layers.Dot(axes=[1, 1])([q_attention, q_one_tensors])
+        x_out = self._make_output_layer()(score)
+        
+        self._backend = tensorflow.keras.Model(inputs=[query, doc, freq_vec, d_one_tensors, q_one_tensors], outputs=x_out)
